028479d70bcd284f0d6f5c768c2aef956832aa02
sreelekhab77 <sreelekha789sl@gmail.com>
2024-02-06 12:29:39 +0530

    first commit

---
+++ src/test/java/google/cicddemo/googleautomation.java
@@ -0,0 +1,24 @@
+package google.cicddemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class googleautomation {
+
+@Test
+public static void launch() throws InterruptedException
+{
+		// TODO Auto-generated method stub
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver =new FirefoxDriver();
+		driver.get("https://.cm/");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+				
+		}
+}
