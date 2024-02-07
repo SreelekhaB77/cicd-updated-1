645432045a7204818dc445a1e5fb0d043211ff5d
sreelekhab77 <133074494+SreelekhaB77@users.noreply.github.com>
2024-02-07 12:07:55 +0530

    Update googleautomation.java

--- src/test/java/google/cicddemo/googleautomation.java
+++ src/test/java/google/cicddemo/googleautomation.java
@@ -1,35 +1,34 @@
 package google.cicddemo;
-
+ 
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+ 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
-
+ 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+ 
 public class googleautomation {
-
+	public static RemoteWebDriver driver;
+ 
+@SuppressWarnings("deprecation")
 @Test
 public static void launch() throws InterruptedException
 {
-		// TODO Auto-generated method stub
+ 
+	System.out.println("hi");		
 	
-		// WebDriverManager.firefoxdriver().setup();
-		// WebDriver driver =new FirefoxDriver();
-
-
-	FirefoxProfile firefoxProfile = new FirefoxProfile();File extension = new File("extension.xpi");
-firefoxProfile.addExtension(extension);
-DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-capabilities.setCapability(FirefoxDriver.PROFILE, firefoxProfile);
-driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
-	
-
-	
-		driver.get("https://www.google.co.in/");
-		driver.manage().window().maximize();
-		Thread.sleep(2000);
-				
-		}
+    WebDriverManager.firefoxdriver().setup();
+    driver = new FirefoxDriver();
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+    driver.get("https://www.google.com/");
+}	
 }
